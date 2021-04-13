f4af4c3c8b52a95e36a6190f456a3b6f55794da5
MuresanIA <muresan.alexandru933@gmail.com>
2021-04-13 21:17:28 +0300

    practice

--- DOM/script.js
+++ DOM/script.js
@@ -1,38 +1,9 @@
-// DOM Selectors
-// --------------
-// getElementsByTagName
-// getElementsByClassName
-// getElementById
-
-// querySelector
-// querySelectorAll
-
-// getAttribute
-// setAttribute
-
-// ##Changing Styles
-// style.{property} //ok
-
-// className //best
-// classList //best
-
-// classList.add
-// classList.remove
-// classList.toggle
-
-// ##Bonus
-// innerHTML //DANGEROUS
-
-// parentElement
-// children
-
-// ##It is important to CACHE selectors in variables
-
-
-//script for listeners
-
-var button = document.getElementsByTagName("button")[0];
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
 
 button.addEventListener("click", function(){
-    console.log("CLICK!!!");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode("testing"));
+    ul.appendChild(li);
 });
